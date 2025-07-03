dd109ca6a4b761d06f41a46e488f51199a50caf9
Mike Hsieh <mike.tsaichan.hsieh@gmail.com>
2025-07-03 18:45:09 +0800

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-60 h-fit flex flex-col">
+      <div className="">Order Book</div>
+    </div>
+  );
+}
+
+export default App;
